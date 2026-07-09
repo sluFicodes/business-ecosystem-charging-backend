b24dc69e0edfe4d95a2734aa6664b18028f5f879
SHENGXING LU <slu@SHENGXINGs-MacBook-Air.local>
2026-07-09 12:30:19 +0200

    copyright headers added

--- src/wstore/charging_engine/schedulers_by_http.py
+++ src/wstore/charging_engine/schedulers_by_http.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2013 - 2016 CoNWeT Lab., Universidad Politécnica de Madrid
 # Copyright (c) 2026 Future Internet Consulting and Development Solutions S.L.
 
 # This file belongs to the business-charging-backend
@@ -9,6 +10,14 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
 
